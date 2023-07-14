a5736545598680ec9a665e2114d94c89c35f499f
zikotan <tananzakaria630@gmail.com>
2023-07-14 14:22:20 -0700

    response

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alp[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; al[i] != '\0'; i++)
+		putchar(alp[i]);
+	putchar('\n');
+	return (0);
+}
