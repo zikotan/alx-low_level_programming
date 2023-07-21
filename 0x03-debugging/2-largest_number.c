aba7d744bdd396302501a134207b8ae6794c4ee7
zikotan <tananzakaria630@gmail.com>
2023-07-21 05:24:19 -0700

    response

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,10 +16,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (a > b && c > a)
-	{
-		largest = c;
-	}
 	else if (b > c)
 	{
 		largest = b;
@@ -31,4 +27,3 @@ int largest_number(int a, int b, int c)
 	
 	return (largest);
 }
-
