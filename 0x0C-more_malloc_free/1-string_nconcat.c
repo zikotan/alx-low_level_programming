b40e8d5fa51561c92836c0846dd2edf399f458d9
zikotan <tananzakaria630@gmail.com>
2023-08-10 14:45:32 -0700

    response

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (n < l2)
 		s0 = malloc(sizeof(char) * (l1 + n + 1));
 	else
-		s0 = malloc(sizeof(char) * (l1 + len2 + 1));
+		s0 = malloc(sizeof(char) * (l1 + l2 + 1));
 
 	if (!s0)
 		return (NULL);
