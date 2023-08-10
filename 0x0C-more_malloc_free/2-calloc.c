53ba7b713766ac27a42816842cd5bbffb5ae74d9
zikotan <tananzakaria630@gmail.com>
2023-08-10 14:54:41 -0700

    response

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *mem - fills memory with a const byte
+ * @s0: the memory area to be filled
+ * @a: the char to copy
+ * @n: the number of times to copy b
+ *
+ * Return: a pointer to the memory area s0
+ */
+char *mem(char *s0, char a, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s0[i] = a;
+	}
+
+	return (s0);
+}
+
+/**
+ * *callocation - allocates memory for an array
+ * @nmemb: the number of elements in the array
+ * @size: the size of each element
+ *
+ * Return: a pointer to the allocated memory
+ */
+void *callocation(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	p = malloc(size * nmemb);
+
+	if (p == NULL)
+		return (NULL);
+
+	mem(p, 0, nmemb * size);
+
+	return (p);
+}
+
