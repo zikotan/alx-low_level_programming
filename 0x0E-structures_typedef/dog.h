020f71cdc041a8c3e01bf92f7922b6a30ae93341
zikotan <tananzakaria630@gmail.com>
2023-08-14 18:33:48 -0700

    response

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a basic info of dog
+ * @name: the First member
+ * @age: the Second member
+ * @owner: the Third member
+ *
+ * Description: Longer description
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
