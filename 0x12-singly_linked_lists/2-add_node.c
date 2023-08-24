fd12ec767e3de4199512b2c156d762a56b93c904
zikotan <tananzakaria630@gmail.com>
2023-08-24 13:53:26 -0700

    response

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * add_node - it adds a new node at the beginning of a linked list
+ * @head: the double pointer to the list_t list
+ * @str: the new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_l;
+	unsigned int length = 0;
+
+	while (str[length])
+		length++;
+
+	new_l = malloc(sizeof(list_t));
+	if (!new_l)
+		return (NULL);
+
+	new_l->str = strdup(str);
+	new_l->length = length;
+	new_l->next = (*head);
+	(*head) = new_l;
+
+	return (*head);
+}
+
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,13 +2,12 @@
 #define LISTS_H
 
 /**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
+ * struct list_s - the singly linked list
+ * @str: the string - (malloc'ed string)
+ * @len: the length of the string
+ * @next: it points to the next node
  *
- * Description: singly linked list node structure
- * for Holberton project
+ * Description: the singly linked list node structure
  */
 typedef struct list_s
 {
@@ -19,9 +18,8 @@ typedef struct list_s
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
+void free_list(list_t *head);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
-
 #endif
 
