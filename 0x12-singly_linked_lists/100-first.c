e33b7cebca223552d846d9039b0da561dc2c93a2
zikotan <tananzakaria630@gmail.com>
2023-08-24 14:49:07 -0700

    response

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,5 +8,5 @@ void intr(void) __attribute__((constructor));
 void intr(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n);
+	printf("I bore my house upon my back!\n");
 }
