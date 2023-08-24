1d75db45ef8f641a68f56b57ce6d64fbcce31c32
zikotan <tananzakaria630@gmail.com>
2023-08-24 14:21:24 -0700

    response

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * first - it prints a sentence before the main
+ */
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
