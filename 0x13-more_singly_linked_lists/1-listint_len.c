71183c4b4f7804a4961ccfcb0188a8eb3093e754
zikotan <tananzakaria630@gmail.com>
2023-08-29 13:14:57 -0700

    response

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * listint_len - it returns the number of elms in a linked_lists
+ * @h: the linked_list of type listint_t to traverse
+ *
+ * Return: the number of nodes
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h)
+	{
+		n++;
+		h = h->next;
+	}
+
+	return (num);
+}
+
