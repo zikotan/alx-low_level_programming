4dec6acecfe370bd1589c2e47b46447d78fec34d
zikotan <tananzakaria630@gmail.com>
2023-09-04 15:54:27 -0700

    response

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
+
+/**
+ * create_buffer - it allocates 1024 bytes for a buffer
+ * @file: the name of the file buffer is storing chars for
+ *
+ * Return: the pointer to the new_allocation buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buf;
+
+	buf = malloc(sizeof(char) * 1024);
+
+	if (buf == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
