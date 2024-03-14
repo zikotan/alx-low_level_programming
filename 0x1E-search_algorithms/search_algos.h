505c7b21faa00005dc6041fa997e7fe61b31a064
zikotan <tananzakaria630@gmail.com>
2024-03-14 01:22:53 +0000

    response

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -47,5 +47,4 @@ int advanced_binary(int *array, size_t size, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
 
-
-#endif /* SEARCH_ALGO_H */
+#endif
