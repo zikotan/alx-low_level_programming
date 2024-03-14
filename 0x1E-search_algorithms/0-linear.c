3715237b3c5fecd99b5c51f738d25c51006a76ff
zikotan <tananzakaria630@gmail.com>
2024-03-14 00:56:23 +0000

    response

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,5 @@
 #include "search_algos.h"
+#include <unistd.h>
 
 /**
   * linear_search - func
@@ -8,19 +9,23 @@
   *
   * Return: int
   */
+
 int linear_search(int *array, size_t size, int value)
 {
-	size_t i;
+    size_t i;
+    char buffer[50]; // Assuming maximum length of output string
 
-	if (array == NULL)
-		return (-1);
+    if (array == NULL)
+        return (-1);
 
-	for (i = 0; i < size; i++)
-	{
-		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
-		if (array[i] == value)
-			return (i);
-	}
+    for (i = 0; i < size; i++)
+    {
+        int length = snprintf(buffer, sizeof(buffer), "Value checked array[%ld] = [%d]\n", i, array[i]);
+        write(1, buffer, length); // Write to standard output
+        if (array[i] == value)
+            return (i);
+    }
 
-	return (-1);
+    return (-1);
 }
+
